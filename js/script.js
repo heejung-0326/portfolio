04150d1fe2016ead289b9f476804444d383dca88
HEEJUNG <duqwlqdk@gmail.com>
2025-07-14 19:51:56 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,69 +1,64 @@
 $(function(){
-  // 헤더 메뉴 클릭시
-  $('.gnb li').removeClass('active').eq(0).addClass('active');
 
-  const sectionIds = [
-    '#intro', '#about', '#project'
-  ];
+  $(window).on('scroll', function(){
+    // 헤더 배경
+    if($(window).scrollTop() > 80){
+      $('header').addClass('header-bg');
+    } else {
+      $('header').removeClass('header-bg');
+    }
 
-  $('#intro').show();
-  $('#about, #project').hide();
+    // about 애니메이션
+    const aboutTop = $('#about').offset().top;
+    const scrollBottom = $(window).scrollTop() + $(window).height();
+    if (scrollBottom > aboutTop + 100) {
+      $('.about-item').each(function(idx) {
+        setTimeout(() => {
+          $(this).addClass('show');
+        }, idx * 200);
+      });
+    }
 
-
-  $('.gnb li a').on('click', function(e){
-    e.preventDefault();
-    const idx = $(this).parent().index();
-
-
-    $('#intro, #about, #project').fadeOut(300);
-    setTimeout (()=>{
-      $(sectionIds[idx]).fadeIn(300);
-    },300);
+    // contact 애니메이션
+    const contactTop = $('.contact_inner').offset().top;
+    if (scrollBottom > contactTop + 100) {
+      $('.contact_inner').addClass('active');
+    }
   });
 
-  //모달
-  $('.pop').click(function(){
-    $('.modal').fadeIn(300);
-    $('.close_btn').click(function(){
-      $('.modal').fadeOut(300, function(){
-        $('#intro').fadeIn(300);
-        $('.gnb li').removeClass('active').eq(0).addClass('active');
-      });
-    });
-  });
+  // //프로젝트 슬라이드
+  // let slide = 0;
+  // const slides = $('.slide');
+  // const slideCount = slides.length;
 
-  //프로젝트 슬라이드
-  let slide = 0;
-  const slides = $('.slide');
-  const slideCount = slides.length;
+  // slides.hide().eq(0).show();
 
-  slides.hide().eq(0).show();
+  // $('.slide-next').click(function(){
+  //   if(slide < slideCount-1){
+  //     slides.eq(slide).fadeOut(300,function(){
+  //       slide++;
+  //       slides.eq(slide).fadeIn(300);
+  //     });
+  //   } else {
+  //     slides.eq(slide).fadeOut(300, function(){
+  //       slide = 0;
+  //       slides.eq(slide).fadeIn(300);
+  //     });
+  //   }
+  // });
 
-  $('.slide-next').click(function(){
-    if(slide<slideCount-1){
-      slides.eq(slide).fadeOut(300,function(){
-        slide++;
-        slides.eq(slide).fadeIn(300);
-      });
-  }else{
-    slides.eq(slide).fadeOut(300, function(){
-      slide =0;
-      slides.eq(slide).fadeIn(300);
-    });
-  }
-});
+  // $('.slide-prev').click(function(){
+  //   if(slide > 0){
+  //     slides.eq(slide).fadeOut(300,function(){
+  //       slide--;
+  //       slides.eq(slide).fadeIn(300);
+  //     });
+  //   } else {
+  //     slides.eq(slide).fadeOut(300, function(){
+  //       slide = slideCount - 1;
+  //       slides.eq(slide).fadeIn(300);
+  //     });
+  //   }
+  // });
 
-$('.slide-prev').click(function(){
-  if(slide>0){
-    slides.eq(slide).fadeOut(300,function(){
-      slide--;
-      slides.eq(slide).fadeIn(300);
-    });
-  }else{
-    slides.eq(slide).fadeOut(300, function(){
-      sldie = slideCount -1;
-      slides.eq(slide).fadeIn(300);
-    });
-  }
-});
 });
